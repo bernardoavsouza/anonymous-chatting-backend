da75bc2cafcea816a76c1581e646f2455e2afcd1
bernardoavsouza <bernardoavsouza@gmail.com>
2024-10-04 01:50:01 -0300

    refactor: refactor index test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,17 @@
 import { wsServer } from './config/server';
 import { io as Client, Socket } from 'socket.io-client';
 import { createServer } from 'http';
+import { ServerUrl } from './utils/server-utils';
 
 describe('Ensure that the websocket server working fine', () => {
   let client: Socket;
   const httpServer = createServer();
-  const protocol = process.env.protocol || 'http';
-  const host = process.env.host || 'localhost';
-  const port = +(process.env.port || '3000');
+  const serverUrl = ServerUrl.getInstance();
 
   beforeAll((done) => {
     wsServer.attach(httpServer);
-    httpServer.listen(port, host, () => {
-      client = Client(`${protocol}://${host}:${port}`);
+    httpServer.listen(serverUrl.port, serverUrl.host, () => {
+      client = Client(serverUrl.url);
       done();
     });
   });
