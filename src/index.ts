4931815ad5ed4b5d766d09463b35b8a16be08081
bernardoavsouza <bernardoavsouza@gmail.com>
2025-03-16 00:52:50 -0300

    feat: add hello world

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { wsServer } from './config/servers/ws-server';
 
 wsServer.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('message', () => {
+    wsServer.emit('message', 'Hello world!');
   });
 });
