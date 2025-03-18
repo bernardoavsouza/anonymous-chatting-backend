1c21661e15ed62098dc4147e4337298609e20352
bernardoavsouza <bernardoavsouza@gmail.com>
2025-03-18 00:26:28 -0300

    chore: fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
-export default [
-  {
-    files: ['**/*.{js,mjs,cjs,ts}'],
-  },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
-];
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintPluginPrettier,
+  { ignores: ['**/node_modules/**', '**/dist/**'] },
+  { files: ['src/**.ts'] },
+);
