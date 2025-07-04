073eb35a6af117f76855102f4ac427199431254c
bernardoavsouza <bernardoavsouza@gmail.com>
2025-07-03 22:50:23 -0300

    chore: add fastify as http platform

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import type { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
