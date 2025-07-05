ddd5123e9bc5cc28ff453b9f62070312a1b7f934
bernardoavsouza <bernardoavsouza@gmail.com>
2025-07-04 22:58:05 -0300

    chore: make server be listened outiside container

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
