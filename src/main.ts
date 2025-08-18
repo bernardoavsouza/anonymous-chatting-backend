0219223f1deefc89cb59963c3397ff4b8a8dc254
bernardoavsouza <bernardoavsouza@gmail.com>
2025-08-17 23:56:11 -0300

    fix: fix lint complaint

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import type { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
