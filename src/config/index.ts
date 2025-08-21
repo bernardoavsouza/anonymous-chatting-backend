e81e3db3bb557eeed99cf588d0f546520e756b8f
bernardoavsouza <bernardoavsouza@gmail.com>
2025-08-21 16:54:23 -0300

    fix(redis): fix env issues

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,16 +1,5 @@
-export default (): Record<string, unknown> => {
-  const host = process.env.REDIS_HOST;
-  const port = process.env.REDIS_PORT;
-  const username = process.env.REDIS_USERNAME;
-  const password = process.env.REDIS_PASSWORD;
-  const url = `redis://${username}:${password}@${host}:${port}`;
-  return {
-    redis: {
-      host,
-      port,
-      username,
-      password,
-      url,
-    },
-  };
-};
+import redisConfig from './redis';
+
+export default (): Record<string, unknown> => ({
+  ...redisConfig(),
+});
