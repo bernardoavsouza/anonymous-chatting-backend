40338a46b679ba1eef1c96a8390444ab59f41ee4
bernardoavsouza <cienty@MacBook-Pro-de-Bernardo.local>
2026-03-29 14:50:12 -0300

    test: enhance mocks

--- __mocks__/ioredis.ts
+++ __mocks__/ioredis.ts
@@ -1,4 +1,18 @@
-export default jest.fn(() => ({
-  rpush: jest.fn(),
-  del: jest.fn(),
-}));
+export default jest.fn(() => {
+  const store = new Map<string, string>();
+
+  return {
+    rpush: jest.fn().mockResolvedValue(0),
+    del: jest.fn().mockImplementation((key: string) => {
+      store.delete(key);
+      return Promise.resolve(1);
+    }),
+    set: jest.fn().mockImplementation((key: string, value: string) => {
+      store.set(key, value);
+      return Promise.resolve(undefined);
+    }),
+    get: jest.fn().mockImplementation((key: string) => {
+      return Promise.resolve(store.get(key) ?? null);
+    }),
+  };
+});
